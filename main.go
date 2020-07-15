e53774486086236555e9d7f1b6943e2d4445fc2d
Bayu Waskita <bayu.waskita@geekseat.com.au>
2020-07-15 22:53:21 +0700

    Add flag option when launching

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/joho/godotenv"
 	"log"
@@ -11,21 +12,39 @@ import (
 func main() {
 	godotenv.Load()
 
+	var portNum string
+	flag.StringVar(&portNum, "port", "", "number of lines to read from the file")
+
+	var fileLocation string
+	flag.StringVar(&fileLocation, "location", "", "File Location")
+	flag.Parse()
+
 	static_file_path := "./public"
-	if path := os.Getenv("FILE_PATH"); path != ""{
+	if path := os.Getenv("FILE_PATH"); path != "" {
 		static_file_path = path
+	}
+
+	if fileLocation != "" {
+		fmt.Println("input detected, Changing location to ", fileLocation)
+		static_file_path = fileLocation
 	}
 
 	fs := http.FileServer(http.Dir(static_file_path))
 	http.Handle("/", fs)
 
 	port := "3000"
-	if port_env := os.Getenv("PORT"); port_env != ""{
+	if port_env := os.Getenv("PORT"); port_env != "" {
+		fmt.Println("Loading Configuration from file .env")
 		port = port_env
 	}
 
-	log.Printf("Listening on :%v...",port)
-	err := http.ListenAndServe(fmt.Sprintf(":%v",port), nil)
+	if portNum != "" {
+		fmt.Println("input detected, Changing port to ", portNum)
+		port = portNum
+	}
+
+	log.Printf("Listening on :%v...", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
